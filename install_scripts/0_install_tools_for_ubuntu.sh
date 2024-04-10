bfafe0c5ac38ac6e865cf4652ac6d49d1eb3a392
Hugo Dupras <jabesq@gmail.com>
2024-04-10 11:32:52 +0200

    Add install script for ubuntu

---
+++ install_scripts/0_install_tools_for_ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+
+printf "Install all build dependencies"
+sudo apt-get update; sudo apt-get -y install stow build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl make \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+printf "Install pyenv"
+curl https://pyenv.run | bash
+
+printf "Install docker"
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+printf "Install Helm"
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get -y install helm
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -9,11 +9,10 @@ fi
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 # export PATH=${HOME}/bash_autosetup/bin:${PATH}
 # export PATH="/Users/jabesq/.local/bin:$PATH"
-# export PATH="$HOME/.pyenv/bin:$PATH"
 # export PATH=$HOME/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/Users/jabesq/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
